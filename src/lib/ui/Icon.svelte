22163cda78321368305cce88a7612355d61e508e
Slav Basharov <slavbacharov@gmail.com>
2022-11-19 13:38:23 +0100

    Added Icon component

---
+++ src/lib/ui/Icon.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let icon;
+    export let color = "white";
+    export let onClick = (e) => {};
+</script>
+
+<iconify-icon {icon} style={`color:${color}`} on:click={onClick} />
+
+<style lang="scss">
+    iconify-icon {
+        padding: 6px;
+        font-size: 15px;
+        border-radius: 4px;
+        &:hover {
+            background-color: rgba(0, 0, 0, 0.187);
+        }
+    }
+</style>
