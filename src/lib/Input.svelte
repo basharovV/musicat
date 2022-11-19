be805cc00ec6689df7a8dd7e16ffff84553727ae
Slav Basharov <slavbacharov@gmail.com>
2022-11-19 14:11:23 +0100

    Updated autocomplete handing on Input component
    - Option for tab to behave as Enter
    - minimal style option

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,21 +1,67 @@
 <script>
+    import { onMount } from "svelte";
+
     export let value;
     export let onChange = (evt) => {};
     export let fullWidth = false;
     export let autoCompleteValue = null;
+    export let tabBehavesAsEnter = false;
     export let onEnterPressed = null;
+    export let onBackspacePressed = null;
+    export let onEscPressed = null;
+    export let autoFocus = false;
+    export let placeholder = "";
+    export let minimal = false;
 
     function onKeyDown(evt) {
         if (evt.keyCode === 13) {
             if (onEnterPressed) {
+                evt.preventDefault();
                 onEnterPressed();
+            }
+        } else if (evt.keyCode === 9) {
+            // Tab
+            if (autoCompleteValue?.length) {
+                evt.preventDefault();
+                if (tabBehavesAsEnter) {
+                    onEnterPressed && onEnterPressed();
+                } else {
+                    value = autoCompleteValue;
+                }
+            }
+        } else if (evt.keyCode === 27) {
+            if (autoCompleteValue?.length) {
+                // Esc
+                evt.preventDefault();
+                onEscPressed && onEscPressed();
+            }
+        } else if (evt.keyCode === 8) {
+            if (value?.length === 0) {
+                // Backspace on empty
+                evt.preventDefault();
+                onBackspacePressed && onBackspacePressed();
             }
         }
     }
+
+    let inputField;
+    onMount(() => {
+        if (autoFocus) inputField.focus();
+    });
 </script>
 
 <div>
-    <input bind:value on:input={onChange} class:full-width={fullWidth} on:keydown={onKeyDown}/>
+    <input
+        bind:this={inputField}
+        bind:value
+        {placeholder}
+        on:input={onChange}
+        class:full-width={fullWidth}
+        class:minimal
+        on:keydown={onKeyDown}
+        spellcheck="false"
+        autocomplete="off"
+    />
 
     {#if autoCompleteValue}
         <p class="autocomplete">{autoCompleteValue}</p>
@@ -40,6 +86,14 @@
         &.full-width {
             width: 100%;
         }
+        &.minimal {
+            background: none;
+            border: none;
+            outline: none;
+        }
+        &::placeholder {
+            opacity: 0.5;
+        }
     }
 
     .autocomplete {
@@ -48,7 +102,7 @@
         left: 6px;
         right: 0px;
         bottom: 0px;
-        top: 0px;
+        top: 2px;
         max-width: 100%;
         text-overflow: ellipsis;
         white-space: nowrap;
