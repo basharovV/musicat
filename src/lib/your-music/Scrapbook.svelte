353b3ad0c6a794f659b2fa1725470d55424ff767
Slav Basharov <slavbacharov@gmail.com>
2022-11-19 13:50:18 +0100

    Scrapbook improvements - tag cloud, file format filter

--- src/lib/your-music/Scrapbook.svelte
+++ src/lib/your-music/Scrapbook.svelte
@@ -24,10 +24,61 @@
     import { flip } from "svelte/animate";
     import { quadInOut } from "svelte/easing";
     import { getLinkItemWithData } from "../../utils/URLMetadata";
+    import TagCloud from "./TagCloud.svelte";
+    import MenuInput from "../menu/MenuInput.svelte";
+    import hotkeys from "hotkeys-js";
 
-    $: contentItems = liveQuery(async () => {
+    let contentTypes = [
+        {
+            icon: "bi:file-earmark-text",
+            name: "lyrics"
+        },
+        {
+            icon: "bi:file-earmark-play",
+            name: "audio"
+        },
+        {
+            icon: "dashicons:editor-video",
+            name: "video"
+        },
+        {
+            icon: "akar-icons:link-chain",
+            name: "link"
+        }
+    ];
+
+    let selectedContentTypes = [];
+
+    function toggleContentType(contentType: string) {
+        if (selectedContentTypes.includes(contentType)) {
+            selectedContentTypes.splice(
+                selectedContentTypes.findIndex((t) => t === contentType),
+                1
+            );
+        } else {
+            selectedContentTypes.push(contentType);
+        }
+        selectedContentTypes = selectedContentTypes;
+    }
+
+    $: allContentItems = liveQuery(async () => {
         return await db.scrapbook.toArray();
     });
+
+    $: filteredContentItems =
+        $allContentItems?.filter((item) => {
+            let matchesContentType = selectedContentTypes.length
+                ? selectedContentTypes.includes(item?.fileType?.type)
+                : true;
+            let matchesTags = selectedTags.length ? false : true;
+            if (selectedTags.length) {
+                const matchingTags = item.tags.filter((i) =>
+                    selectedTags.includes(i)
+                );
+                matchesTags = matchingTags.length > 0;
+            }
+            return matchesTags && matchesContentType;
+        }) ?? [];
 
     function addFile(filePath) {
         console.log("adding item", filePath);
@@ -177,11 +228,12 @@
 
     // Right-click menu
 
-    let rightClickedItem: ArtistContentItem;
+    let contextItem: ArtistContentItem;
     let isConfirmingDelete = false;
 
     function closeMenu() {
-        rightClickedItem = null;
+        contextItem = null;
+        tagEditorVisible = false;
         isConfirmingDelete = false;
     }
 
@@ -190,9 +242,9 @@
             isConfirmingDelete = true;
             return;
         }
-        if (rightClickedItem?.id) {
+        if (contextItem?.id) {
             // Delete the item
-            db.scrapbook.delete(rightClickedItem.id);
+            db.scrapbook.delete(contextItem.id);
             closeMenu();
         }
     }
@@ -200,7 +252,7 @@
     let rightClickedPos = { x: 0, y: 0 };
 
     function onRightClick(e, item) {
-        rightClickedItem = item;
+        contextItem = item;
         rightClickedPos = { x: e.clientX, y: e.clientY };
     }
 
@@ -210,6 +262,54 @@
             addLink(data);
         }
     }
+
+    $: tags =
+        $allContentItems?.reduce((tags, item) => {
+            item.tags.forEach((t) => {
+                if (!tags.includes(t)) {
+                    tags.push(t);
+                }
+            });
+            return tags;
+        }, []) ?? [];
+
+    let selectedTags = [];
+
+    let tagEditorVisible = false;
+    let tagUserInput = "";
+    function showTagEditor(e: MouseEvent, item: ArtistContentItem) {
+        contextItem = item;
+        tagEditorVisible = true;
+        rightClickedPos = { x: e.clientX, y: e.clientY };
+    }
+
+    function deleteTag(tag) {
+        contextItem.tags.splice(
+            contextItem.tags.findIndex((t) => t === tag),
+            1
+        );
+        contextItem = contextItem;
+        db.scrapbook.update(contextItem.id, {
+            tags: contextItem.tags
+        });
+    }
+    function addTagToContextItem() {
+        if (tagAutoCompleteValue?.length) {
+            tagUserInput = tagAutoCompleteValue;
+        }
+        db.scrapbook.update(contextItem.id, {
+            tags: [...contextItem.tags, tagUserInput.toLowerCase().trim()]
+        });
+        tagUserInput = "";
+        contextItem = null;
+        tagEditorVisible = false;
+    }
+    $: tagAutoCompleteValue =
+        tagUserInput.length && tags?.find((v) => v.startsWith(tagUserInput));
+
+    hotkeys("esc", function (event, handler) {
+        closeMenu();
+    });
 </script>
 
 <container
@@ -221,27 +321,22 @@
     class:file-drop={$hoveredFiles.length}
 >
     <div class="legend">
-        <div class="lyrics">
-            <iconify-icon icon="bi:file-earmark-text" />
-            <p>Lyrics</p>
-        </div>
-        <div class="audio">
-            <iconify-icon icon="bi:file-earmark-play" />
-            <p>Audio</p>
-        </div>
-        <div class="video">
-            <iconify-icon icon="dashicons:editor-video" />
-            <p>Video</p>
-        </div>
-        <div class="link">
-            <iconify-icon icon="akar-icons:link-chain" />
-            <p>Link</p>
-        </div>
+        {#each contentTypes as contentType}
+            <div
+                class:selected={selectedContentTypes.includes(contentType.name)}
+                class={contentType.name}
+                on:click={() => toggleContentType(contentType.name)}
+            >
+                <iconify-icon icon={contentType.icon} />
+                <p>{contentType.name}</p>
+            </div>
+        {/each}
     </div>
+    <TagCloud {tags} bind:selectedTags />
     <div class="scrapbook">
-        {#if $contentItems}
+        {#if filteredContentItems}
             <div>
-                {#each $contentItems as item (item.id)}
+                {#each filteredContentItems as item (item.id)}
                     <div
                         animate:flip={{ duration: 180, easing: quadInOut }}
                         class="item"
@@ -254,6 +349,20 @@
                         {:else if item.type === "link"}
                             <LinkBlock {item} />
                         {/if}
+
+                        <div class="tags">
+                            {#each item.tags as tag}
+                                <p class:selected={selectedTags.includes(tag)}>
+                                    {tag}
+                                </p>
+                            {/each}
+                            <div
+                                class="add-tag"
+                                on:click={(e) => showTagEditor(e, item)}
+                            >
+                                <p>edit tags</p>
+                            </div>
+                        </div>
                     </div>
                 {/each}
                 {#if $hoveredFiles.length}
@@ -268,19 +377,40 @@
     </div>
 </container>
 
-{#if rightClickedItem}
+{#if contextItem && !tagEditorVisible}
     <Menu
         x={rightClickedPos.x}
         y={rightClickedPos.y}
         onClickOutside={closeMenu}
     >
-        <MenuOption isDisabled={true} text={rightClickedItem.name} />
+        <MenuOption isDisabled={true} text={contextItem.name} />
         <MenuOption
             isDestructive={true}
             isConfirming={isConfirmingDelete}
             onClick={deleteItem}
             text={"Delete item"}
             confirmText="Click again to delete"
+        />
+    </Menu>
+{/if}
+
+{#if contextItem && tagEditorVisible}
+    <Menu
+        x={rightClickedPos.x}
+        y={rightClickedPos.y}
+        onClickOutside={closeMenu}
+    >
+        <MenuOption isDisabled={true} text="Edit tags" />
+        {#each contextItem.tags as tag}
+            <MenuOption text={tag} onDelete={() => deleteTag(tag)} />
+        {/each}
+        <MenuInput
+            bind:value={tagUserInput}
+            autoCompleteValue={tagAutoCompleteValue}
+            onEnterPressed={addTagToContextItem}
+            autoFocus
+            placeholder="Add a tag"
+            onEscPressed={closeMenu}
         />
     </Menu>
 {/if}
@@ -308,10 +438,28 @@
             flex-direction: row;
             align-items: center;
             gap: 5px;
+            &:hover {
+                p {
+                    color: rgb(217, 214, 214);
+                    border-bottom: 1px solid rgb(154, 149, 149);
+                }
+            }
+            &.selected {
+                p {
+                    color: white;
+                    border-bottom: 1px solid white;
+                }
+            }
+
             p {
+                box-sizing: border-box;
+                border-bottom: 1px solid transparent;
                 font-size: 13px;
                 margin: 0;
                 color: rgb(171, 171, 171);
+                cursor: default;
+                user-select: none;
+                text-transform: capitalize;
             }
             &.lyrics > iconify-icon {
                 color: rgb(212, 212, 66);
@@ -340,18 +488,65 @@
         button {
             margin-top: 2em;
         }
-        p {
+        > p {
             opacity: 0.5;
         }
     }
     .item {
         height: auto;
         width: auto;
+        max-width: 165px;
 
         &.placeholder {
             border: 1px dashed white;
             border-radius: 4px;
             padding: 1em;
+        }
+
+        .tags {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            column-gap: 5px;
+            justify-content: right;
+            align-items: center;
+            min-height: 23px;
+            > p {
+                cursor: default;
+                font-size: 12px;
+                line-height: 18px;
+                margin: 0;
+                width: fit-content;
+                border-radius: 3px;
+                color: rgb(168, 151, 145);
+                border: 1px solid transparent;
+                padding: 0 0.3em;
+
+                &.selected {
+                    color: #d5cdf0;
+                    text-shadow: 0px 2px 10px #c0bbd0;
+                }
+            }
+            .add-tag {
+                p {
+                    margin: 0;
+                    font-size: 12px;
+                    line-height: 12px;
+                    cursor: default;
+                    box-sizing: border-box;
+                    color: #685c8a;
+                    &:hover {
+                        opacity: 0.8;
+                    }
+                    &:active {
+                        opacity: 0.5;
+                    }
+                }
+            }
+
+            .tag-input {
+                position: absolute;
+            }
         }
     }
 </style>
---
+++ src/lib/your-music/TagCloud.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+    export let tags = [
+    ];
+    export let selectedTags = [];
+    function selectTag(tag: string) {
+        if (selectedTags.includes(tag)) {
+            selectedTags.splice(
+                selectedTags.findIndex((t) => t === tag),
+                1
+            );
+        } else {
+            selectedTags.push(tag);
+        }
+        selectedTags = selectedTags;
+    }
+</script>
+
+<div class="tag-cloud">
+    {#each tags as tag}
+        <div
+            class="tag"
+            class:selected={selectedTags.includes(tag)}
+            on:click={() => selectTag(tag)}
+        >
+            <p>{tag}</p>
+        </div>
+    {/each}
+</div>
+
+<style lang="scss">
+    .tag-cloud {
+        margin: 1em 0;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 5px;
+        justify-content: stretch;
+    }
+
+    .tag {
+        border-radius: 20px;
+        padding: 1px 10px;
+        background-color: rgba(0, 0, 0, 0.346);
+        box-sizing: border-box;
+        border: 1px solid transparent;
+
+        &:hover {
+            border: 1px solid rgb(140, 140, 140);
+        }
+
+        &:active {
+            opacity: 0.5;
+        }
+
+        &.selected {
+            color: #d1c5f5;
+            border-color: #5f32e7;
+            background-color: #5123dd27;
+        }
+
+        p {
+            margin: 0;
+            user-select: none;
+            cursor: default;
+        }
+    }
+</style>
