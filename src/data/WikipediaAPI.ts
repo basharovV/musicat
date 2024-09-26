18fdb52bd7669c5a4ba4079b91cce6b298c1d97b
Slav Basharov <slavbacharov@gmail.com>
2024-09-26 22:21:22 +0200

    More accurate Wiki article lookup (via DBpedia)
    - Now searching for exact match, and only if the entity is an artist or band

--- src/data/WikipediaAPI.ts
+++ src/data/WikipediaAPI.ts
@@ -1,36 +1,24 @@
-import WBK from "wikibase-sdk";
-
 export async function getWikipediaUrlForArtist(artistName) {
     if (!artistName?.length) return null;
 
     const validStatements = ["P27", "P495"];
 
     try {
-        const wdk = WBK({
-            instance: "https://www.wikidata.org",
-            sparqlEndpoint: "https://query.wikidata.org/sparql"
-        });
-        let url = wdk.searchEntities({ search: artistName });
-
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log("Wiki response", json);
-        const firstItem =
-            json.search && json.search.find((s) => s.match.type === "label");
-
-        if (!firstItem) return null;
-
-        url = wdk.getEntities({
-            ids: [firstItem.id]
-        });
-        const { entities } = await fetch(url).then((res) => res.json());
-        console.log(entities);
-        const firstEntity = Object.values(entities)[0];
-        if (!firstEntity) return null;
-        const siteLink = firstEntity?.["sitelinks"]?.enwiki?.title;
+        const dbpediaResult = await (
+            await fetch(
+                `https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=SELECT+%3Fartist+%3Flink%0D%0AWHERE+%7B%0D%0A++%7B+%3Fartist+a+dbo%3ABand+%7D%0D%0A++UNION%0D%0A++%7B+%3Fartist+a+dbo%3AMusicalArtist+%7D%0D%0A++%3Fartist+foaf%3Aname+%22${encodeURIComponent(artistName)}%22%40en+.%0D%0A++%3Fartist+foaf%3AisPrimaryTopicOf+%3Flink+.%0D%0A%7D%0D%0ALIMIT+1%0D%0A&format=application%2Fsparql-results%2Bjson&timeout=10000&signal_void=on&signal_unconnected=on`
+            )
+        ).json();
+        console.log("dbpedia", dbpediaResult);
+        let siteLink = false;
+        for (let item of dbpediaResult?.results?.bindings) {
+            if (item?.link?.value) {
+                siteLink = item.link.value;
+            }
+        }
 
         if (!siteLink) return null;
-        return `https://en.wikipedia.org/wiki/${siteLink}?useskin=vector-2022`;
+        return `${siteLink}?useskin=vector-2022`;
     } catch (error) {
         console.error("Error fetching data from Wikipedia API:", error);
         return null;
