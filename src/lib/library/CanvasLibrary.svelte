d2eb281c7a1284a939a422b1645ce879929ca452
Baptiste Augrain <daiyam@zokugun.org>
2024-12-17 17:13:58 +0100

    enhance: improve themes

--- src/lib/library/CanvasLibrary.svelte
+++ src/lib/library/CanvasLibrary.svelte
@@ -2193,8 +2193,7 @@
                                                             scaleY: 0.36,
                                                             data: "M33 7.64c-1.34-2.75-5.2-5-9.69-3.69A9.87 9.87 0 0 0 18 7.72a9.87 9.87 0 0 0-5.31-3.77C8.19 2.66 4.34 4.89 3 7.64c-1.88 3.85-1.1 8.18 2.32 12.87C8 24.18 11.83 27.9 17.39 32.22a1 1 0 0 0 1.23 0c5.55-4.31 9.39-8 12.07-11.71c3.41-4.69 4.19-9.02 2.31-12.87",
                                                             fill:
-                                                                $currentSong?.id ===
-                                                                song.id
+                                                                $currentSong?.id === song.id
                                                                     ? $currentThemeObject[
                                                                           "library-playing-icon"
                                                                       ]
@@ -2225,7 +2224,14 @@
                                                             scaleY: 0.36,
                                                             data: "M33 7.64c-1.34-2.75-5.2-5-9.69-3.69A9.87 9.87 0 0 0 18 7.72a9.87 9.87 0 0 0-5.31-3.77C8.19 2.66 4.34 4.89 3 7.64c-1.88 3.85-1.1 8.18 2.32 12.87C8 24.18 11.83 27.9 17.39 32.22a1 1 0 0 0 1.23 0c5.55-4.31 9.39-8 12.07-11.71c3.41-4.69 4.19-9.02 2.31-12.87",
                                                             fill: "transparent",
-                                                            stroke: "#784bff"
+                                                            stroke: 
+                                                                $currentSong?.id === song.id
+                                                                    ? $currentThemeObject[
+                                                                          "library-playing-icon"
+                                                                      ]
+                                                                    : $currentThemeObject[
+                                                                          "library-favourite-hover-icon"
+                                                                      ]
                                                         }}
                                                     />
                                                 {/if}
