a94dbc5cf2a4cb8725d9fc227be036fcb7266051
Baptiste Augrain <daiyam@zokugun.org>
2025-01-23 15:45:11 +0100

    perf: load album's tracks only once

--- src/lib/albums/util.ts
+++ src/lib/albums/util.ts
@@ -1,23 +1,7 @@
-import type { Album } from "../../App";
-import { db } from "../../data/db";
-
 export const HEADER_HEIGHT = 22;
 export const PADDING = 14;
 export const ROW_HEIGHT = 26;
 
-export async function getAlbumDetailsHeight(album: Album): Promise<number> {
-    var tracks = await db.songs
-        .where("id")
-        .anyOf(album.tracksIds)
-        .sortBy("trackNumber");
-
-    const height =
-        PADDING +
-        300 +
-        HEADER_HEIGHT +
-        tracks.length * ROW_HEIGHT +
-        8 +
-        PADDING;
-
-    return height;
+export async function getAlbumDetailsHeight(length: number): Promise<number> {
+    return PADDING + 300 + HEADER_HEIGHT + length * ROW_HEIGHT + 8 + PADDING;
 }
