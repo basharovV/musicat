504c90518b438434617fe7a324c9aa6291125edf
Slav Basharov <slavbacharov@gmail.com>
2025-09-08 23:52:47 +0200

    fix: open in finder/explorer not working
    - Updated to use plugin opener plugin instead of shell

--- src/lib/menu/file.ts
+++ src/lib/menu/file.ts
@@ -1,7 +1,7 @@
-import { open } from "@tauri-apps/plugin-shell";
+import { openPath } from "@tauri-apps/plugin-opener";
 import type { Song } from "../../App";
 
 export function openInFinder(song: Song) {
     const query = song.path.replace(song.file, "");
-    open(query);
+    openPath(query);
 }
